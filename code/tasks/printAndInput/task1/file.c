7bef787ba4cdca10edb259ede902f409b94671ac
andrey <xxxkeep3rxxx@gmail.com>
2022-10-20 11:59:50 +0300

    20.10

---
+++ code/tasks/printAndInput/task1/file.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+//Написать программу, которая выводит на экран ваши имя и фамилию.
+int main(){
+    printf("Nikonov Andrey\n");
+    return 0;
+}
---
+++ code/tasks/printAndInput/task7/file.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//Вывести размеры основных типов данных
+
+int main(){
+    printf("char = 8bit\n");
+    printf("short = 16bit\n");
+    printf("int = 16bit or 32bit\n");
+    printf("long = 32bit\n");
+    printf("long long = 64bit\n");
+    printf("float = 32bit\n");
+    printf("double = 64bit\n");
+
+    return 0;
+}
